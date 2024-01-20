b24a457e0ea027ba2f5aa3a88c74369450389969
Octavio Santana <osq.005@gmail.com>
2024-01-20 19:21:09 +0000

    end section 25

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    AuthModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {
+
+}
